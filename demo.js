f8577d5f097a74bf7ab0f61d598e1698c7e3cf9a
Mike <korostelevm@gmail.com>
2021-11-02 14:45:44 -0400

    demo

--- demo.js
+++ demo.js
@@ -3,8 +3,32 @@ const Monkey = require('monkey-typewriter')
 const app = express()
 app.get('/', (req, res) => {
      console.log('received a [GET] ')
-     res.json({
-          'monkey says': Monkey.word()
-     })
+
+     let w  = Monkey.word()
+     let l = w.length - 4;
+     let fill = (c,n)=>{
+          return Array.apply(null, Array(n)).map(l=>{
+               return c
+          }).join('')
+     }
+
+     let monkey = `
+                             .="=.
+                      _/.-.-.\_     _
+                     ( ( o o ) )    ))
+                      |/  "  \|    //
+      .-${fill('-',l)}------.        \'---'/    //
+     _| ${w} |_       /'"""'\\  ((
+   =(_|_${fill('-',l)}______|_)=    / /_,_\ \\  \\
+     |::${fill(':',l)}:::::::|      \_\\_'__/ \  ))
+     |::${fill(':',l)}:::::[]|       /'  /'~\  |//
+     |o=${fill('=',l)}======.|      /   /    \  /
+     '""${fill('"',l)}"""""""'  '--',--'\/\    /
+                   '-- "--'  '--'
+                   `
+
+     res.send(`<pre>${monkey}</pre>`)
 })
+
+
 app.listen(process.env.PORT)
